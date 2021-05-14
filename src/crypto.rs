9dce4b8ca345eeb39b2ec6b8529a4cddb32a3447
BearGuy <stephen@peterkins.ca>
2021-05-13 20:35:46 -0400

    remove extraneous hash function

--- src/crypto.rs
+++ src/crypto.rs
@@ -8,9 +8,3 @@ pub fn hash(data: Vec<u8>) -> [u8; 32] {
     hasher.update(data);
     hasher.finalize().as_slice().try_into().unwrap()
 }
-
-pub fn hash_data(data: Vec<u8>, output: &mut [u8]) {
-    let mut hasher = Sha256::new();
-    hasher.update(data);
-    output.copy_from_slice(hasher.finalize().as_slice())
-}
