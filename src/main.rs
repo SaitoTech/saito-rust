071e1449fb0b5917ae09d4cf0d0db9ef41d29d96
Stephen Peterkins <stephen@peterkins.ca>
2021-05-19 12:14:17 -0400

    Tokio runtime and basic consensus runtime implementation (#29)
    * feat: first server impl with structure
    
    * refactor: change server naming scheme to consensus for greater accuracy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use saito_rust::consensus;
+use tokio::signal;
+
+#[tokio::main]
+pub async fn main() -> saito_rust::Result<()> {
+    consensus::run(signal::ctrl_c()).await
 }
