c3c73bac248b448740196ff79f834350632c374d
BearGuy <stephen@peterkins.ca>
2021-05-20 13:14:10 -0400

    refactor: block headers and block indexs used in blockchain

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,11 +1,11 @@
-use crate::block::Block;
-use std::collections::HashMap;
+use crate::block::{Block, BlockHeader};
 
+pub type BlockIndex = (BlockHeader, [u8; 32]);
 /// Indexes of chain attribute
 #[derive(Debug, Clone)]
 pub struct BlockchainIndex {
     /// Vector of blocks
-    blocks: Vec<Block>,
+    blocks: Vec<BlockIndex>,
 }
 
 impl BlockchainIndex {
@@ -23,31 +23,12 @@ impl BlockchainIndex {
 pub struct Blockchain {
     /// Index of `Block`s
     index: BlockchainIndex,
-    /// Hash map of all `Block`s received, both on and off the longest chain
-    block_hash_longest_chain_hashmap: HashMap<[u8; 32], u8>,
-    /// Hash map of the ids of `Block` hashes
-    block_hash_block_id_hashmap: HashMap<[u8; 32], u64>,
-
-    /// Position of the longest chain
-    longest_chain_position: usize,
-    /// Flag indicating if the lognest chain is set
-    longest_chain_is_set: bool,
-
     /// The start of the blockchain UNIx timestamp
     genesis_timestamp: u64,
     /// The latest `Block` id in the genesis period
     genesis_block_id: u64,
     /// The amount of `Block`s kept on chain
     genesis_period: u64,
-
-    /// Last `Block` hash
-    last_block_hash: [u8; 32],
-    /// Last `Block` id
-    last_block_id: u64,
-    /// Last `Block` timestamp
-    last_timestamp: u64,
-    /// Last `Block` burnfee
-    last_burnfee: f64,
 }
 
 impl Blockchain {
@@ -55,29 +36,15 @@ impl Blockchain {
     pub fn new() -> Self {
         Blockchain {
             index: BlockchainIndex::new(),
-            block_hash_longest_chain_hashmap: HashMap::new(),
-            block_hash_block_id_hashmap: HashMap::new(),
-
-            longest_chain_position: 0,
-            longest_chain_is_set: false,
-
             genesis_timestamp: 0,
             genesis_block_id: 0,
             genesis_period: 0,
-
-            last_block_hash: [0; 32],
-            last_block_id: 0,
-            last_timestamp: 0,
-            last_burnfee: 0.0,
         }
     }
 
     /// Returns the latest `Block` as part of the longest chain
-    pub fn get_latest_block(&self) -> Option<&Block> {
-        match !self.longest_chain_is_set {
-            true => None,
-            false => Some(&self.index.blocks[self.longest_chain_position]),
-        }
+    pub fn get_latest_block(&self) -> Option<&BlockIndex> {
+        self.index.blocks.last()
     }
 
     /// Append `Block` to the index of `Blockchain`
@@ -88,20 +55,8 @@ impl Blockchain {
             return;
         }
 
-        let pos: usize = self.index.blocks.len();
-        self.block_hash_block_id_hashmap
-            .insert(block.block_hash(), block.id());
-        self.block_hash_longest_chain_hashmap
-            .insert(block.block_hash(), 1);
-
-        self.index.blocks.push(block);
-
-        self.last_block_hash = self.index.blocks[pos].block_hash();
-        self.last_timestamp = self.index.blocks[pos].timestamp();
-        self.last_block_id = self.index.blocks[pos].id();
-
-        self.longest_chain_position = pos;
-        self.longest_chain_is_set = true;
+        let block_index: BlockIndex = (block.header().clone(), block.hash());
+        self.index.blocks.push(block_index);
     }
 }
 
@@ -111,26 +66,16 @@ mod tests {
     use super::*;
     use crate::block::Block;
     use crate::keypair::Keypair;
-    use std::collections::HashMap;
 
     #[test]
     fn blockchain_test() {
         let blockchain = Blockchain::new();
 
         assert_eq!(blockchain.index.blocks, vec![]);
-        assert_eq!(blockchain.block_hash_longest_chain_hashmap, HashMap::new());
-        assert_eq!(blockchain.block_hash_block_id_hashmap, HashMap::new());
-        assert_eq!(blockchain.longest_chain_position, 0);
-        assert_eq!(blockchain.longest_chain_is_set, false);
 
         assert_eq!(blockchain.genesis_timestamp, 0);
         assert_eq!(blockchain.genesis_block_id, 0);
         assert_eq!(blockchain.genesis_period, 0);
-
-        assert_eq!(blockchain.last_block_hash, [0; 32]);
-        assert_eq!(blockchain.last_block_id, 0);
-        assert_eq!(blockchain.last_timestamp, 0);
-        assert_eq!(blockchain.last_burnfee, 0.0);
     }
     #[test]
     fn blockchain_get_latest_block_none_test() {
@@ -148,8 +93,8 @@ mod tests {
         blockchain.add_block(block.clone());
 
         match blockchain.get_latest_block() {
-            Some(prev_block) => {
-                assert_eq!(prev_block.clone(), block);
+            Some((prev_block_header, _)) => {
+                assert_eq!(&prev_block_header.clone(), block.header());
                 assert!(true);
             }
             None => assert!(false),
@@ -161,21 +106,8 @@ mod tests {
         let block = Block::new(Keypair::new().public_key().clone(), [0; 32]);
 
         blockchain.add_block(block.clone());
+        let (block_header, _) = blockchain.index.blocks[0].clone();
 
-        assert_eq!(blockchain.index.blocks[0], block.clone());
-
-        assert_eq!(blockchain.last_block_hash, block.block_hash());
-        assert_eq!(blockchain.last_timestamp, block.timestamp());
-        assert_eq!(blockchain.last_block_id, block.id());
-
-        assert!(blockchain
-            .block_hash_block_id_hashmap
-            .contains_key(&block.block_hash()));
-        assert!(blockchain
-            .block_hash_longest_chain_hashmap
-            .contains_key(&block.block_hash()));
-
-        assert_eq!(blockchain.longest_chain_position, 0);
-        assert_eq!(blockchain.longest_chain_is_set, true);
+        assert_eq!(block_header, *block.clone().header());
     }
 }
