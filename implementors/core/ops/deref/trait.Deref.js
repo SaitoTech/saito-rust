ddcb518b6142d230277175d31271266e12fd4732
clayrab <claytonrabenda@gmail.com>
2021-06-04 07:59:16 +0000

    Deploying to gh-pages from @ SaitoTech/saito-rust@189f26ce68ac43801f8317d3aca934eba6d14d2b 🚀

---
+++ implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["saito_rust"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"saito_rust/blockchain/struct.BLOCKCHAIN.html\" title=\"struct saito_rust::blockchain::BLOCKCHAIN\">BLOCKCHAIN</a>","synthetic":false,"types":["saito_rust::blockchain::BLOCKCHAIN"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
