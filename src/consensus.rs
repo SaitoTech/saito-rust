d54dfdf5e0a40b997c64124462b0ac2d825f3274
Clayton Rabenda <claytonrabenda@gmail.com>
2021-06-08 22:09:44 +0800

    add_block without clone

--- src/consensus.rs
+++ src/consensus.rs
@@ -103,36 +103,37 @@ impl Consensus {
                     SaitoMessage::TryBundle => {
                         BLOCKCHAIN.with(|blockchain_rc| {
                             let blockchain: &mut Blockchain = &mut *blockchain_rc.borrow_mut();
-                            if let Some(block) = mempool.process(message, blockchain.latest_block())
-                            {
-                                match blockchain.add_block(block.clone()) {
-                                    AddBlockEvent::AcceptedAsLongestChain => {
-                                        println!("AcceptedAsLongestChain");
-                                        block_tx
-                                            .send(SaitoMessage::NewBlock {
-                                                payload: block.hash().clone(),
-                                            })
-                                            .unwrap();
-                                    }
-                                    fail_message => {
-                                        println!("WE MISSED LONGEST CHAIN, WHAT HAPPENED?");
-                                        println!("{:?}", fail_message)
-                                    }
-                                }
-                            }
-                            // let block = mempool.process(message, blockchain.latest_block()).unwrap();
-                            // let block_hash = block.hash().clone();
-                            // match blockchain.add_block(block) {
-                            //     AddBlockEvent::AcceptedAsLongestChain => {
-                            //         block_tx
-                            //             .send(SaitoMessage::NewBlock { payload: block_hash })
-                            //             .unwrap();
-                            //     }
-                            //     fail_message => {
-                            //         println!("WE MISSED LONGEST CHAIN, WHAT HAPPENED?");
-                            //         println!("{:?}", fail_message)
+                            // if let Some(block) = mempool.process(message, blockchain.latest_block())
+                            // {
+                            //     match blockchain.add_block(block) {
+                            //         AddBlockEvent::AcceptedAsLongestChain => {
+                            //             println!("AcceptedAsLongestChain");
+                            //             block_tx
+                            //                 .send(SaitoMessage::NewBlock {
+                            //                     payload: block.hash().clone(),
+                            //                 })
+                            //                 .unwrap();
+                            //         }
+                            //         fail_message => {
+                            //             println!("WE MISSED LONGEST CHAIN, WHAT HAPPENED?");
+                            //             println!("{:?}", fail_message)
+                            //         }
                             //     }
                             // }
+                            let block = mempool.process(message, blockchain.latest_block()).unwrap();
+                            let block_hash = block.hash().clone();
+                            match blockchain.add_block(block) {
+                                AddBlockEvent::AcceptedAsLongestChain => {
+                                    println!("AcceptedAsLongestChain");
+                                    block_tx
+                                        .send(SaitoMessage::NewBlock { payload: block_hash })
+                                        .unwrap();
+                                }
+                                fail_message => {
+                                    println!("WE MISSED LONGEST CHAIN, WHAT HAPPENED?");
+                                    println!("{:?}", fail_message)
+                                }
+                            }
                         })
                     }
                     _ => {}
