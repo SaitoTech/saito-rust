ac0ee2e3ff02a73251f0f2a4eb66eb0d2ca86b3d
escapedcat <github@htmlcss.de>
2021-06-13 11:06:58 +0800

    feat(keypairstore): try to add clap to handle cli #39

--- src/keypairstore.rs
+++ src/keypairstore.rs
@@ -1,4 +1,24 @@
-use std::env;
+use clap::Clap;
+// use std::env;
+// use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+// #[derive(StructOpt)]
+// struct Cli {
+//     /// The pattern to look for
+//     pattern: String,
+//     /// The path to the file to read
+//     #[structopt(parse(from_os_str))]
+//     path: std::path::PathBuf,
+// }
+
+#[derive(Clap, Debug)]
+#[clap(name = "saito")]
+struct PwFile {
+    /// Path to pw-file
+    #[clap(short, long, default_value = "/spme/default/pw/path")]
+    path: String,
+}
 
 #[derive(Debug, Clone)]
 pub struct KeyPairStore {}
@@ -7,22 +27,36 @@ impl KeyPairStore {
     /// Create new `KeyPairStore`
     pub fn new() -> Self {
         println!("----> KEYPAIRSTORE");
-        let args: Vec<String> = env::args().collect();
+        // let args = Cli::from_args();
+        // let args: Vec<String> = env::args().collect();
 
-        println!("arguments: {}", args.len());
+        let pw_file = PwFile::parse();
 
-        if args.len() > 1 {
-            println!("argument given");
-            let command = args[1].clone();
+        println!("PwFile {}!", pw_file.path);
 
-            if command == "foo" {
-                println!("argument is foo");
-                let foo_arg = args[2].clone();
-                println!("FOO is: {}", &foo_arg.to_string());
-            }
-        } else {
-            println!("no arguments given");
-        }
+        // println!("arguments: {}", args.len());
+
+        // "./saito_rust --pw-file=/myencryptedusb/saitopw"
+        // then it should read from that, otherwise it should read from
+        // a standard location(maybe ./data/passwd?).
+        // If --pw-file isn't passed and the standard location is empty, then it should say something like
+        // - "no password file found, creating one at ./data/passwd, please enter a password:".
+        // - "Loading password from {location which user has provided}, please enter password:"
+        // - "Loading password from {standardize location on the system}, please enter password:"
+        // - "Password file not found, creating one at {standardize location on the system}, please enter password:"
+
+        // if args.len() > 1 {
+        //     println!("argument given");
+        //     let command = args[1].clone();
+
+        //     if command == "pw-file" {
+        //         println!("argument is `pw-file`");
+        //         let arg_pw_file = args[2].clone();
+        //         println!("pw-file is: {}", &arg_pw_file.to_string());
+        //     }
+        // } else {
+        //     println!("no arguments given");
+        // }
 
         KeyPairStore {}
     }
