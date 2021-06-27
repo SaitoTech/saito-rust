fd1e49404121cbc978652b20a2230d450e32c532
escapedcat <github@htmlcss.de>
2021-06-27 11:29:27 +0800

    feat(keypairstore): file match result #39

--- src/keypairstore.rs
+++ src/keypairstore.rs
@@ -1,71 +1,49 @@
+extern crate rpassword;
+
 use clap::Clap;
-use std::fs;
-// use std::env;
-// use structopt::StructOpt;
+use std::fs::File;
+use std::io;
+use std::io::Read;
 
-/// Search for a pattern in a file and display the lines that contain it.
-// #[derive(StructOpt)]
-// struct Cli {
-//     /// The pattern to look for
-//     pattern: String,
-//     /// The path to the file to read
-//     #[structopt(parse(from_os_str))]
-//     path: std::path::PathBuf,
-// }
-
-// cargo run -- --path boom
+// cargo run -- --key-path boom
 
 #[derive(Clap, Debug)]
 #[clap(name = "saito_rust")]
-struct PwFile {
-    /// Path to pw-file
-    #[clap(short, long, default_value = "./passwd")]
-    path: String,
+struct KeyFile {
+    /// Path to key-file
+    #[clap(short, long, default_value = "./keyFile")]
+    key_path: String,
 }
 
 #[derive(Debug, Clone)]
 pub struct KeyPairStore {}
 
-// clippy says:
-// impl Default for keypairstore::KeyPairStore {
-//     fn default() -> Self {
-//         Self::new()
-//     }
-// }
-
 impl KeyPairStore {
     /// Create new `KeyPairStore`
     pub fn new() -> Self {
         println!("----> KEYPAIRSTORE");
-        // let args = Cli::from_args();
-        // let args: Vec<String> = env::args().collect();
 
-        let pw_file = PwFile::parse();
+        let key_file = KeyFile::parse();
 
-        println!("PwFile path: {}", pw_file.path);
+        println!("KeyFile path: {}", key_file.key_path);
 
-        // let password_path = if pw_file.path.is_empty() {
-        //     println!("did not got pw-file path?");
-        //     "./passwd"
-        // } else {
-        //     println!("got pw-file path?");
-        //     &pw_file.path
-        // }
-        // .to_string();
+        let data = || -> Result<String, io::Error> {
+            let mut f = File::open(key_file.key_path)?;
+            let mut s = String::new();
+            f.read_to_string(&mut s)?;
+            Ok(s)
+        };
 
-        // let data = || -> Result<(), MyError> {
-        //     fs::read_to_string(pw_file.path).expect("Unable to read password file")?;
-        //     Ok(())
-        // };
-        // if let Err(_err) = data() {
-        //     println!("Failed to perform necessary steps");
-        // }
-
-        let data = fs::read_to_string(pw_file.path).expect("Unable to read password file");
-
-        println!("PW File content: {}", data);
-
-        // println!("arguments: {}", args.len());
+        let _content = match data() {
+            Err(_e) => {
+                // panic!("{}", e)
+                KeyPairStore::create_key_file()
+            }
+            Ok(s) => {
+                println!("PW File content: {}", s);
+                s
+            }
+        };
 
         // "./saito_rust --pw-file=/myencryptedusb/saitopw"
         // then it should read from that, otherwise it should read from
@@ -76,19 +54,17 @@ impl KeyPairStore {
         // - "Loading password from {standardize location on the system}, please enter password:"
         // - "Password file not found, creating one at {standardize location on the system}, please enter password:"
 
-        // if args.len() > 1 {
-        //     println!("argument given");
-        //     let command = args[1].clone();
-
-        //     if command == "pw-file" {
-        //         println!("argument is `pw-file`");
-        //         let arg_pw_file = args[2].clone();
-        //         println!("pw-file is: {}", &arg_pw_file.to_string());
-        //     }
-        // } else {
-        //     println!("no arguments given");
-        // }
-
         KeyPairStore {}
+    }
+
+    pub fn create_key_file() -> String {
+        // - ask for pw
+        // - write pw to file to given path
+
+        println!("Let's create a file");
+        // "my_secure_password".to_owned()
+        let pass = rpassword::prompt_password_stdout("Password: ").unwrap();
+        println!("Your password is {}", pass);
+        pass.to_string()
     }
 }
