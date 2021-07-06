710d26e76979ada3986076e658b7ee23e24dac59
escapedcat <github@htmlcss.de>
2021-07-06 18:09:16 +0800

    feat(keypairstore): write file to path #39

--- src/keypairstore.rs
+++ src/keypairstore.rs
@@ -35,7 +35,7 @@ impl KeyPairStore {
         println!("KeyFile path: {}", key_file.key_path);
 
         let data = || -> Result<String, io::Error> {
-            let mut f = File::open(key_file.key_path)?;
+            let mut f = File::open(&key_file.key_path)?;
             let mut s = String::new();
             f.read_to_string(&mut s)?;
             Ok(s)
@@ -46,7 +46,7 @@ impl KeyPairStore {
                 // panic!("{}", e)
                 // @QUESTION: why is this failing if i leave out the expect?
                 // because the type doesn't math then but why? when to know to do what?
-                KeyPairStore::create_key_file().expect("Failed to create file!")
+                KeyPairStore::create_key_file(&key_file.key_path).expect("Failed to create file!")
             }
             Ok(s) => {
                 println!("PW File content: {}", s);
@@ -54,29 +54,17 @@ impl KeyPairStore {
             }
         };
 
-        // "./saito_rust --pw-file=/myencryptedusb/saitopw"
-        // then it should read from that, otherwise it should read from
-        // a standard location(maybe ./data/passwd?).
-        // If --pw-file isn't passed and the standard location is empty, then it should say something like
-        // - "no password file found, creating one at ./data/passwd, please enter a password:".
-        // - "Loading password from {location which user has provided}, please enter password:"
-        // - "Loading password from {standardize location on the system}, please enter password:"
-        // - "Password file not found, creating one at {standardize location on the system}, please enter password:"
-
         KeyPairStore {}
     }
 
-    pub fn create_key_file() -> Result<String, io::Error> {
-        // - ask for pw
-
-        // println!("File does not exist. Let's create it!");
+    pub fn create_key_file(key_file_path: &str) -> Result<String, io::Error> {
+        println!("File does not exist. Let's create it!");
         let password = rpassword::prompt_password_stdout("Password: ").unwrap();
         println!("Your password is {}", password);
 
-        //btw, for directly reading/writing a whoel fiel to/from a string, there is std::fs::read_to_string and std::Fs::write
+        // btw, for directly reading/writing a whoel fiel to/from a string, there is std::fs::read_to_string and std::Fs::write
 
-        // @TODO: - write pw to file to given path
-        let mut file = File::create("foo.txt")?;
+        let mut file = File::create(key_file_path)?;
         file.write_all(password.as_bytes())?;
         Ok(password)
     }
