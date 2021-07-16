61434b1f1463867c05045d49e144def1104d5312
BearGuy <stephen@peterkins.ca>
2021-07-16 15:46:01 +0000

    Deploying to gh-pages from @ SaitoTech/saito-rust@3b0979402dd3ff13d7203f4141bdd8f0fd401407 🚀

--- saito_rust/blockchain/sidebar-items.js
+++ saito_rust/blockchain/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["bit_pack",""],["bit_unpack",""],["run",""]],"struct":[["Blockchain",""]]});
+initSidebarItems({"fn":[["bit_pack",""],["bit_unpack",""],["run",""]],"struct":[["Blockchain",""]],"type":[["UtxoSet",""]]});
