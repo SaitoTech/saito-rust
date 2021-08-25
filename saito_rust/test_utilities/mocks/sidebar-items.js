329d52aa72ed963a1e945bc6a988fe959c74d8d3
clayrab <claytonrabenda@gmail.com>
2021-08-25 07:45:31 +0000

    Deploying to gh-pages from @ SaitoTech/saito-rust@9b201cc01efa669727c12413b8945b31b66d0bf5 🚀

--- saito_rust/test_utilities/mocks/sidebar-items.js
+++ saito_rust/test_utilities/mocks/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["make_mock_block",""],["make_mock_blockchain",""],["make_mock_tx",""]]});
+initSidebarItems({"fn":[["add_random_block",""],["add_vip_block",""],["make_mock_block",""],["make_mock_blockchain",""],["make_mock_tx",""]],"struct":[["MockTimestampGenerator",""]]});
