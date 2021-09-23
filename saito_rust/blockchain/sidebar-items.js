945dbc7b27c172d843234c5400925e6b0f5aed90
trevelyan <david.lancashire@gmail.com>
2021-09-23 03:25:04 +0000

    Deploying to gh-pages from @ SaitoTech/saito-rust@7186db04bc5ce106bffeb16dff95f7f6572397af 🚀

--- saito_rust/blockchain/sidebar-items.js
+++ saito_rust/blockchain/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["GENESIS_PERIOD",""],["MAX_STAKER_RECURSION",""],["MAX_TOKEN_SUPPLY",""],["PRUNE_AFTER_BLOCKS",""]],"fn":[["bit_pack",""],["bit_unpack",""],["run",""]],"struct":[["Blockchain",""]],"type":[["UtxoSet",""]]});
+initSidebarItems({"constant":[["GENESIS_PERIOD",""],["MAX_STAKER_RECURSION",""],["MAX_TOKEN_SUPPLY",""],["MIN_GOLDEN_TICKETS_DENOMINATOR",""],["MIN_GOLDEN_TICKETS_NUMERATOR",""],["PRUNE_AFTER_BLOCKS",""]],"fn":[["bit_pack",""],["bit_unpack",""],["run",""]],"struct":[["Blockchain",""]],"type":[["UtxoSet",""]]});
