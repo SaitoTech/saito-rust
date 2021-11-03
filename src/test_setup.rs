4b71df43cf2b225d92a4175fc850e04bc4f5f296
Sanka Darshana <sankadarshana@gmail.com>
2021-11-03 16:05:01 +0530

    feat: added a custom logger instance to unit tests
    replaced simple_logger with env_logger to support env var (RUST_LOG) based log filtering

--- src/test_setup.rs
+++ src/test_setup.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 #[ctor::ctor]
 fn init_tests() {
-    // initialize a logger only for tests (to capture info and trace logs)
-    simple_logger::SimpleLogger::new().init().unwrap();
+    // initialize a logger only for tests (to capture info and trace logs based on RUST_LOG)
+    env_logger::init();
 }
