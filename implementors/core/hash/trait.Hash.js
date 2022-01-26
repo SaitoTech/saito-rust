5e739625c0566c0f46947a51bce22706869dac35
thaodt <ardtimeit@gmail.com>
2022-01-26 09:37:54 +0000

    Deploying to gh-pages from @ SaitoTech/saito-rust@0e997fc285f7f274bda5ccdec3c094676bb16b52 🚀

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["saito_rust"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"saito_rust/slip/enum.SlipType.html\" title=\"enum saito_rust::slip::SlipType\">SlipType</a>","synthetic":false,"types":["saito_rust::slip::SlipType"]}];
+implementors["saito_rust"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"saito_rust/slip/enum.SlipType.html\" title=\"enum saito_rust::slip::SlipType\">SlipType</a>","synthetic":false,"types":["saito_rust::slip::SlipType"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
